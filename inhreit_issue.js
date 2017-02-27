315f4b92c5da66a4c8008d01cccf819e4a223335
Ying <hongying.guo@ximalaya.com>
2017-02-27 17:24:25 +0800

    继承

--- inhreit_issue.js
+++ inhreit_issue.js
@@ -17,16 +17,15 @@ var ma = new Ma('name of ma');
 ma.say();//name of ma
 
 //继承2
-function Animal2(name) {
+var Animal2 = function(name) {
     this.name = name;
-    this.say = function () {
-        console.log(this.name);
-    }
 }
-
-function Ma2(name) {
-    this.name = name;
-    Animal2.call(this,'name of animal2');
+Animal2.prototype.say = function () {
+    console.log(this.name);
 }
+var Ma2 = function(name) {
+    Animal2.call(this,name);
+}
+Ma2.prototype = Object.create(Animal2.prototype);
 var ma2 = new Ma2('name of ma2');
-ma2.say();//name of animal2
+ma2.say();//name of ma2
